f38a90b0d4974917d3bfbdea91718c937d96a320
Atr0fy <2006lucasns@gmail.com>
2024-08-09 09:07:50 -0300

    gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+#Atividade de Detalhar data e Desconto do INSS
--- obj/Debug/net8.0/Aula01.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aula01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90f8afe9594895661c12962ec58f34470d864a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
